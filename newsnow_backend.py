f6e5e9eade7d7a0901f97d4d27728ac8c0348b1f
Wiiat <36886379+Wiiat@users.noreply.github.com>
2020-07-13 14:26:43 -0400

    Updated kivy icon
    Changed the kivy icon to show newsnow icon

--- newsnow_backend.py
+++ newsnow_backend.py
@@ -1,10 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
-<<<<<<< Updated upstream
-from textblob import TextBlob
-=======
->>>>>>> Stashed changes
 
 
 class WebScraper:
@@ -14,117 +10,6 @@ class WebScraper:
     stocks = ['stocks', 'stock market']
     trump_words = ('trump')
 
-<<<<<<< Updated upstream
-    foxnews = ('foxnews.csv', 'info', 'article-body', 'https://www.foxnews.com/')
-    nypost = ('nypost.csv', 'headline-container', 'entry-content entry-content-read-more', 'https://nypost.com/')
-    dailymail = ('dailymail.csv', 'linkro-darkred', 'articleBody', 'https://www.dailymail.co.uk/')
-    reason = ('reason.csv', 'main', 'entry-content', 'https://reason.com/')
-    bbc = ('bbc.csv', 'media__content', 'story-body__inner', 'https://www.bbc.com/')
-    nytimes = ('nytimes.csv', 'css-15zaaaz eq74mwp0', 'css-1vxca1d e1qksbhf0', 'https://www.nytimes.com/')
-    newsmax = ('newsmax.csv', 'nmLeftColumn', 'mainArticleDiv', 'https://www.newsmax.com/')
-    wtimes = ('wtimes.csv', 'contained', 'font-resizer', 'https://www.washingtontimes.com/')
-    npr = ('npr.csv', 'story-text', 'storytext storylocation linkLocation', 'https://www.npr.org/')
-    usatoday = ('usatoday.csv', 'gnt_cw', 'gnt_ar_b', 'https://www.usatoday.com/')
-    abc = ('abc.csv', 'main-container', 'abcnews', 'https://abcnews.go.com/')
-    nbc = ('nbc.csv', 'layout-container zone-a-margin lead-type--threeUp',
-           'article-body__content', 'https://www.nbcnews.com/')
-    mjones = ('mjones.csv', 'grid', 'entry-content', 'https://www.motherjones.com//')
-    msnbc = ('msnbc.csv', 'layout-container zone-a-margin lead-type--threeUp',
-             'article-body__content', 'https://www.msnbc.com/')
-    vox = ('vox.csv', 'c-entry-box--compact__body', 'l-col__main', 'https://www.vox.com/')
-
-    def keyword_search(self, word_list, file, class1, class2, link):
-        special = ('dailymail.csv', 'bbc.csv', 'nytimes.csv', 'newsmax.csv', 'wtimes.csv', 'abc.csv')
-        special2 = ('newsmax.csv', 'wtimes.csv', 'abc.csv')
-        local_link = requests.get(link).text
-        soup = BeautifulSoup(local_link, 'lxml')
-        if file in special2:
-            bowl = soup.find_all(id=class1)
-        else:
-            bowl = soup.find_all(class_=class1)
-        articles = [items.find_all('a') for items in bowl]
-        articles2 = [a for i in articles for a in i]
-        my_file = open(file, 'w')
-        csv_file = csv.writer(my_file)
-        article_links = set({})
-        for articles in articles2:
-            if 'href' not in str(articles):
-                continue
-            if len(articles['href']) < 5:
-                continue
-            if '/' not in articles['href']:
-                continue
-            elif file in special and '.co' not in articles['href']:
-                new_articles = link + articles['href']
-            elif '//' in articles['href'][:2]:
-                new_articles = 'https://' + articles['href'][2:]
-            elif 'https' in articles['href'][:6] or 'http' in articles['href'][:6]:
-                new_articles = articles['href']
-            else:
-                continue
-            while True:
-                try:
-                    s = 0
-                    article_site = requests.get(new_articles).text
-                    break
-                except requests.exceptions.SSLError:
-                    s = 1
-                    article_site = None
-                    break
-            if s == 1:
-                continue
-            else:
-                pass
-            article_soup = BeautifulSoup(article_site, 'lxml')
-            if file in special2:
-                article_body = article_soup.find(id=class2)
-            else:
-                article_body = article_soup.find(class_=class2)
-            if any(words in articles.text.lower() for words in word_list):
-                if len(articles.text) < 3:
-                    continue
-                elif new_articles in article_links or articles.text in article_links:
-                    continue
-                elif 'getty images' in articles.text.lower():
-                    continue
-                else:
-                    article_links.add(new_articles)
-                    article_links.add(articles.text)
-                    csv_file.writerow([articles.text.strip(), new_articles])
-            elif article_body is None:
-                continue
-            else:
-                article_paragraphs = [p.text for p in article_body.find_all('p')
-                                      if any(words in p.text.lower() for words in word_list)]
-                if len(article_paragraphs) == 0:
-                    continue
-                else:
-                    if new_articles in article_links or articles.text in article_links:
-                        continue
-                    else:
-                        for p in article_paragraphs:
-                            for words in word_list:
-                                count = 0
-                                if words in p.lower():
-                                    count += 1
-                                else:
-                                    continue
-                                if p.lower().count(words) > 1 or count > 2:
-                                    if len(articles.text) < 3:
-                                        continue
-                                    elif 'getty images' in articles.text.lower():
-                                        continue
-                                    else:
-                                        article_links.add(new_articles)
-                                        article_links.add(articles.text)
-                                        csv_file.writerow([articles.text.strip(), new_articles])
-                                else:
-                                    continue
-        my_file.close()
-
-    def keyword_lite(self, word_list, file, class1, link):
-        special = ('dailymail.csv', 'bbc.csv', 'nytimes.csv', 'newsmax.csv', 'wtimes.csv', 'abc.csv')
-=======
     foxnews = ('foxnews.csv', 'info', 'https://www.foxnews.com/')
     nypost = ('nypost.csv', 'headline-container', 'https://nypost.com/')
     dailymail = ('dailymail.csv', 'linkro-darkred', 'https://www.dailymail.co.uk/')
@@ -144,7 +29,6 @@ class WebScraper:
     def keyword_lite(self, word_list, file, class1, link):
         special = ('dailymail.csv', 'bbc.csv', 'nytimes.csv', 'newsmax.csv',
                    'wtimes.csv', 'abc.csv')
->>>>>>> Stashed changes
         special2 = ('newsmax.csv', 'wtimes.csv', 'abc.csv')
         local_link = requests.get(link).text
         soup = BeautifulSoup(local_link, 'lxml')
@@ -189,107 +73,3 @@ class WebScraper:
             else:
                 continue
         my_file.close()
-
-<<<<<<< Updated upstream
-    def title_splitter(self, file, class1, class2, link):
-        special = ('dailymail.csv', 'bbc.csv', 'nytimes.csv', 'newsmax.csv', 'wtimes.csv', 'abc.csv')
-        special2 = ('newsmax.csv', 'wtimes.csv', 'abc.csv')
-        local_link = requests.get(link).text
-        soup = BeautifulSoup(local_link, 'lxml')
-        title = {}
-        duplicates = []
-        if file in special2:
-            bowl = soup.find_all(id=class1)
-        else:
-            bowl = soup.find_all(class_=class1)
-        articles = [items.find_all('a') for items in bowl]
-        articles2 = [a for i in articles for a in i]
-        for articles in articles2:
-            if 'href' not in str(articles):
-                continue
-            if len(articles['href']) < 5:
-                continue
-            if '/' not in articles['href']:
-                continue
-            elif file in special and '.co' not in articles['href']:
-                new_articles = link + articles['href']
-            elif '//' in articles['href'][:2]:
-                new_articles = 'https://' + articles['href'][2:]
-            elif 'https' in articles['href'][:6] or 'http' in articles['href'][:6]:
-                new_articles = articles['href']
-            else:
-                continue
-            article_site = requests.get(new_articles).text
-            article_soup = BeautifulSoup(article_site, 'lxml')
-            if file in special2:
-                article_body = article_soup.find(id=class2)
-            else:
-                article_body = article_soup.find(class_=class2)
-            if article_body is None:
-                continue
-            else:
-                if new_articles in duplicates:
-                    continue
-                else:
-                    title[tuple(articles.text.lower().strip().strip("'s").split(" "))] = new_articles
-                    duplicates.append(new_articles)
-        return title
-
-    def stitle_comparer(self, words1, words2):
-        comparison = set({})
-        count = 0
-        if max(len(words2), len(words1)) == words1:
-            longest = words1
-            shortest = words2
-        else:
-            longest = words2
-            shortest = words1
-        blob1 = TextBlob(" ".join(longest)).noun_phrases
-        blob2 = TextBlob(" ".join(shortest)).noun_phrases
-        for values in blob1:
-            values2 = values.strip("'").split(" ")
-            for i in values2:
-                if len(i) > 2:
-                    comparison.add(i)
-                else:
-                    continue
-        for values in blob2:
-            values2 = values.strip("'").split(" ")
-            for i in values2:
-                if len(i) > 2:
-                    if i in comparison:
-                        count += 1
-                    else:
-                        continue
-                else:
-                    continue
-        if 0 < count < len(blob2):
-            return True
-        else:
-            return False
-
-    def ltitle_comparer(self, x, y):
-        csv_file = csv.writer(open('similar_articles.csv', 'w'))
-        csv_file.writerow(['Article 1', 'Article 2', 'Link 1', 'Link 2'])
-        keys1 = [i for i in x.keys() if i is not None]
-        keys2 = [i for i in y.keys() if i is not None]
-        for k1 in keys1:
-            for k2 in keys2:
-                if self.stitle_comparer(k1, k2) is True:
-                    csv_file.writerow([' '.join(k1), ' '.join(k2), x[k1], y[k2]])
-                else:
-                    continue
-
-=======
->>>>>>> Stashed changes
-
-if __name__ == "__main__":
-    title = WebScraper()
-    '''a = title.title_splitter(title.bbc[0], title.bbc[1], title.bbc[2], title.bbc[3])
-    b = title.title_splitter(title.abc[0], title.abc[1], title.abc[2], title.abc[3])
-    title.ltitle_comparer(a, b)'''
-<<<<<<< Updated upstream
-    title.keyword_lite(title.covid_words, title.foxnews[0], title.foxnews[1], title.foxnews[3])
-=======
-    title.keyword_lite(title.covid_words, title.nypost[0], title.nypost[1], title.nypost[2])
->>>>>>> Stashed changes
--- newsnow_frontend.py
+++ newsnow_frontend.py
@@ -2,10 +2,7 @@ import kivy
 from kivy.app import App, runTouchApp
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
-<<<<<<< Updated upstream
-=======
 from kivy.uix.floatlayout import FloatLayout
->>>>>>> Stashed changes
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -21,10 +18,7 @@ import csv
 import webbrowser
 from kivy.uix.scrollview import ScrollView
 import kivy.modules
-<<<<<<< Updated upstream
-=======
 import time
->>>>>>> Stashed changes
 
 kivy.require("1.11.0")
 Window.clearcolor = (.54, .84, .89, 1)
@@ -56,19 +50,11 @@ class TopicSelectionPage(Screen):
         Clock.schedule_once(self.update_once, 0)
         Clock.schedule_interval(self.update, 1 / 60)
 
-<<<<<<< Updated upstream
-    blm_button = ObjectProperty(None)
-    covid_button = ObjectProperty(None)
-
-=======
     base_widget = ObjectProperty(None)
->>>>>>> Stashed changes
     Button.count = 0
     Button.background_down = ""
     Button.background_normal = ""
     Button.markup = True
-<<<<<<< Updated upstream
-=======
 
     count = ""
 
@@ -80,18 +66,13 @@ class TopicSelectionPage(Screen):
     def pressed(self, button):
         self.count_change(button)
         self.manager.current = "site"
->>>>>>> Stashed changes
 
     def button_collision(self):
         font_instance = 57
         buttons = [self.covid_button, self.blm_button]
         for button in buttons:
             if button.collide_point(*Window.mouse_pos):
-<<<<<<< Updated upstream
-                animation = Animation(font_size=font_instance + 6, s=1/60, duration=.06)
-=======
                 animation = Animation(font_size=font_instance + 6, s=1 / 60, duration=.06)
->>>>>>> Stashed changes
                 button.color = (.96, .60, .61)
                 if button.count == 0:
                     animation.start(button)
@@ -101,8 +82,6 @@ class TopicSelectionPage(Screen):
                 Animation.cancel_all(button)
                 button.color = (1, 1, 1, 1)
                 button.font_size = font_instance
-<<<<<<< Updated upstream
-=======
 
     def update_once(self, dt):
         count = 0
@@ -119,7 +98,6 @@ class TopicSelectionPage(Screen):
                     self.base_widget.add_widget(bttn)
                     break
             count += 1
->>>>>>> Stashed changes
 
     def update(self, dt):
         self.button_collision()
@@ -199,12 +177,6 @@ class NewsSelectionPage(Screen):
         self.button_collision()
 
 
-<<<<<<< Updated upstream
-class FoxNews(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        Clock.schedule_once(self.articles, 0)
-=======
 class Sites(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -212,33 +184,18 @@ class Sites(Screen):
         self.site_csv = ''
         self.next = True
         self.prev = True
->>>>>>> Stashed changes
         Clock.schedule_interval(self.update, 1 / 60)
 
     back_button = ObjectProperty(None)
     menu_button = ObjectProperty(None)
-<<<<<<< Updated upstream
-    news_articles = ObjectProperty(None)
-=======
     refresh_button = ObjectProperty(None)
     news_articles = ObjectProperty(None)
     next_button = ObjectProperty(None)
     previous_button = ObjectProperty(None)
->>>>>>> Stashed changes
 
     buttons = (back_button, menu_button)
     Label.count = 0
 
-<<<<<<< Updated upstream
-    def button_collision(self):
-        font_instance = 26.7
-        buttons = [self.back_button, self.menu_button]
-        for button in buttons:
-            if button.collide_point(*Window.mouse_pos):
-                animation = Animation(font_size=font_instance + 6, s=1 / 60, duration=.06)
-                button.background_color = (1, 1, 1, 1)
-                button.color = (.96, .60, .61, 1)
-=======
     def on_pre_enter(self, *args):
         self.news_articles.clear_widgets()
 
@@ -270,38 +227,22 @@ class Sites(Screen):
                 animation = Animation(font_size=font_instance + 3, s=1 / 60, duration=.06)
                 button.background_color = color1
                 button.color = color2
->>>>>>> Stashed changes
                 if button.count == 0:
                     animation.start(button)
                     button.count += 1
             else:
                 button.count = 0
                 Animation.cancel_all(button)
-<<<<<<< Updated upstream
-                button.color = (1, 1, 1, 1)
-                button.background_color = (.96, .60, .61, 1)
-=======
                 button.background_color = color2
                 button.color = color1
->>>>>>> Stashed changes
                 button.font_size = font_instance
 
     def text_collision(self):
         labels = [i for i in self.news_articles.children]
         for label in labels:
-<<<<<<< Updated upstream
-            if len(label.text) > 190:
-                font_instance = 25
-            else:
-                font_instance = 30
-            label.size = label.texture_size
-            if label.collide_point(*label.to_widget(*Window.mouse_pos)):
-                animation = Animation(font_size=font_instance + 1, s=1 / 60, duration=.06)
-=======
             font_instance = 35
             if label.collide_point(*label.to_widget(*Window.mouse_pos)):
                 animation = Animation(font_size=font_instance + 2, s=1 / 60, duration=.06)
->>>>>>> Stashed changes
                 label.color = (.96, .60, .61, 1)
                 if label.count == 0:
                     animation.start(label)
@@ -312,30 +253,6 @@ class Sites(Screen):
                 label.color = (1, 1, 1, 1)
                 label.font_size = font_instance
 
-<<<<<<< Updated upstream
-    def openlink(self, instance):
-        webbrowser.open(instance, new=2)
-
-    def articles(self, dt):
-        titles = self.csv_load()[0]
-        links = self.csv_load()[1]
-        for lnk, items in zip(links, titles):
-            if len(items.strip()) == 0 or len(lnk.strip()) == 0:
-                continue
-            article_widget = Label(text="[ref={}][b]{}[/b][/ref]".format(lnk, items), markup=True,
-                                   font_size=30, halign='left', text_size=(700, None), size_hint_y=None)
-            self.news_articles.add_widget(article_widget)
-            article_widget.on_ref_press = self.openlink
-
-    def csv_load(self):
-        webscrape = WebScraper()
-        topic_dict = {'covid': webscrape.covid_words, 'blm': webscrape.blm_words}
-        webscrape.keyword_lite(topic_dict[TopicSelectionPage.topic], webscrape.foxnews[0],
-                               webscrape.foxnews[1], webscrape.foxnews[3])
-        titles = []
-        links = []
-        with open('foxnews.csv', 'r') as f:
-=======
     def loading(self):
         self.add_widget(Label(text="[b]Loading...[/b]", markup=True, font_size=60))
         Clock.schedule_once(self.articles, 0)
@@ -367,7 +284,6 @@ class Sites(Screen):
         titles = []
         links = []
         with open(self.site_csv, 'r') as f:
->>>>>>> Stashed changes
             csv_data = list(csv.reader(f))
             for num in range(len(csv_data)):
                 titles.append(csv_data[num][0])
@@ -381,62 +297,6 @@ class Sites(Screen):
         self.text_collision()
 
 
-<<<<<<< Updated upstream
-class NyPost(Screen):
-    pass
-
-
-class DailyMail(Screen):
-    pass
-
-
-class Newsmax(Screen):
-    pass
-
-
-class Reason(Screen):
-    pass
-
-
-class WashingtonTimes(Screen):
-    pass
-
-
-class BBC(Screen):
-    pass
-
-
-class NPR(Screen):
-    pass
-
-
-class USAToday(Screen):
-    pass
-
-
-class ABC(Screen):
-    pass
-
-
-class NBC(Screen):
-    pass
-
-
-class NyTimes(Screen):
-    pass
-
-
-class MotherJones(Screen):
-    pass
-
-
-class MSNBC(Screen):
-    pass
-
-
-class Vox(Screen):
-    pass
-=======
 class FoxNews(Sites):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -542,11 +402,11 @@ class Vox(Sites):
         super().__init__(**kwargs)
         self.webscrape_site = (WebScraper.vox[0], WebScraper.vox[1], WebScraper.vox[2])
         self.site_csv = WebScraper.vox[0]
->>>>>>> Stashed changes
 
 
 class NewsNowApp(App):
     def build(self):
+        self.icon = 'NN-icon.icns'
         kv_file = Builder.load_file("newsnowkivy.kv")
 
         return kv_file
